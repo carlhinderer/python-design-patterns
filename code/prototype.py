f8496b3d15bcc8a4064f346262d4bc48a2d7054a
Carl Hinderer <carl.hinderer4@gmail.com>
2019-07-21 23:28:37 -0600

    Added 'Prototype' class for cloning objects.

--- code/prototype.py
+++ code/prototype.py
@@ -1,3 +1,5 @@
+import copy
+
 class Website: 
     def __init__(self, name, domain, description, author, **kwargs): 
         '''Examples of optional attributes (kwargs): 
@@ -7,20 +9,35 @@ class Website:
         self.domain = domain 
         self.description = description
         self.author = author
-        
         for key in kwargs:
             setattr(self, key, kwargs[key])
  
     def __str__(self): 
         summary = [f'Website "{self.name}"\n',] 
-        
         infos = vars(self).items()
         ordered_infos = sorted(infos)
         for attr, val in ordered_infos:
             if attr == 'name':
                 continue
-            summary.append(f'{attr}: {val}\n')
-            
+            summary.append(f'{attr}: {val}\n')   
         return ''.join(summary)
 
 
+class Prototype: 
+    def __init__(self): 
+        self.objects = dict() 
+ 
+    def register(self, identifier, obj): 
+        self.objects[identifier] = obj 
+ 
+    def unregister(self, identifier): 
+        del self.objects[identifier] 
+ 
+    def clone(self, identifier, **attrs): 
+        found = self.objects.get(identifier) 
+        if not found: 
+            raise ValueError(f'Incorrect object identifier: {identifier}') 
+        obj = copy.deepcopy(found) 
+        for key in attrs:
+            setattr(obj, key, attrs[key])
+        return obj
