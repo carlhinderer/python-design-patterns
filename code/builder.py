6f1d62bd3b4528ea91d4e9db2eefe69500013a3b
Carl Hinderer <carl.hinderer4@gmail.com>
2019-07-21 22:37:21 -0600

    Added the directory for the builder pattern example.

--- code/builder.py
+++ code/builder.py
@@ -94,3 +94,20 @@ class CreamyBaconBuilder:
         time.sleep(self.baking_time)
         self.progress = PizzaProgress.ready
         print('your creamy bacon is ready')
+
+
+class Waiter:
+    def __init__(self):
+        self.builder = None
+
+    def construct_pizza(self, builder):
+        self.builder = builder
+        steps = (builder.prepare_dough, 
+                 builder.add_sauce, 
+                 builder.add_topping, 
+                 builder.bake)
+        [step() for step in steps]
+
+    @property
+    def pizza(self):
+        return self.builder.pizza
